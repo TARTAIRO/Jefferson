554b88ec183627460b437eff5531c30ac31bead8
TARTAIRO <hugo.dutreuil@supinfo.com>
2023-05-15 13:18:03 +0200

    amélioration esthétique

--- jeffersonGUI.py
+++ jeffersonGUI.py
@@ -1,17 +1,3 @@
-"""from tkinter import *
-
-window = Tk()
-window.geometry("1200x700")
-window.title("Cylinder Jefferson")
-window.iconbitmap("th.ico")
-
-
-
-
-
-window.mainloop()
-"""
-
 from tkinter import *
 
 # Définition de la clé de chiffrement
@@ -56,22 +42,24 @@ def decrypt():
 root = Tk()
 root.title("Chiffrement de Jefferson")
 root.iconbitmap("th.ico")
-root.configure(bg='black')
+root.configure(bg='#23272a')
 
-
+# Libellé pour le titre
+title_label = Label(root, text="Chiffrement de Jefferson", fg='#ffffff', bg='#23272a', font=('Helvetica', 20))
+title_label.grid(row=0, column=1, columnspan=3, pady=20)
 
 # Zone de texte pour le texte à chiffrer/déchiffrer
-input_text = Text(root, height=10, width=50,  bg='black', fg="red")
-input_text.grid(row=6, column=1, padx=10, pady=10)
+input_text = Text(root, height=10, width=50, bg='black', fg='red', font=('Helvetica', 12))
+input_text.grid(row=1, column=1, padx=20, pady=10)
 
 # Boutons de chiffrement/déchiffrement
-encrypt_button = Button(root, text="Chiffrer", command=encrypt, bg='black', fg="red")
-encrypt_button.grid(row=6, column=2, padx=10, pady=10)
-decrypt_button = Button(root, text="Déchiffrer", command=decrypt, bg='black', fg="red")
-decrypt_button.grid(row=7, column=2, padx=10, pady=10)
+encrypt_button = Button(root, text="Chiffrer", command=encrypt, bg='#7289da', fg='#ffffff', font=('Helvetica', 12))
+encrypt_button.grid(row=1, column=2, padx=10, pady=10)
+decrypt_button = Button(root, text="Déchiffrer", command=decrypt, bg='#7289da', fg='#ffffff', font=('Helvetica', 12))
+decrypt_button.grid(row=1, column=3, padx=10, pady=10)
 
 # Zone de texte pour le résultat
-output_text = Text(root, height=10, width=50,  bg='black', fg="red")
-output_text.grid(row=6, column=3, columnspan=3, padx=10, pady=10)
+output_text = Text(root, height=10, width=50, bg='black', fg='red', font=('Helvetica', 12))
+output_text.grid(row=2, column=1, columnspan=3, padx=20, pady=10)
 
 root.mainloop()
